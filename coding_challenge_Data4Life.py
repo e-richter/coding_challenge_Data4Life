6cb4a0d8eecbceef01df1612078387fd6f2101a2
eRichter09 <emilius.richter@gmail.com>
2020-06-16 18:41:07 +0200

    adjustments

--- coding_challenge_Data4Life.py
+++ coding_challenge_Data4Life.py
@@ -7,15 +7,12 @@ import multiprocessing as mp
 # creating random email adresses
 domains     = ['gmail.com', 'gmx.de', 'web.de', 'outlook.de', 't-online.de', 'aol.com']
 recipients  = [''.join(random.choice(string.ascii_lowercase) for i in range(7)) + '@'
-               + random.choice(domains) for i in range(10)]
+               + random.choice(domains) for i in range(1000000)]
 
 # function that fakes sending mail by waiting 0.5 seconds
 def send_mail(i):
     time.sleep(0.5)
     print('email was sent to ' + i)
-
-#for i in recipients:
-#    send_mail(i)
 
 # sending mails to every recipient by using multiprocessing
 if __name__ == '__main__':
@@ -23,3 +20,4 @@ if __name__ == '__main__':
     pool = mp.Pool(mp.cpu_count())
     pool.map(send_mail, [i for i in recipients])
     pool.close()
+    
